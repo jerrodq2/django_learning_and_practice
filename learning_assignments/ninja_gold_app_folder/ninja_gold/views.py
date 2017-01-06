3e5dbecd912c5715a9d4561f51b42bb779a3118e
Jerrod Quintana <jerrodq2@yahoo.com>
2017-01-06 17:06:39 -0600

    uploaded the ninja gold practice assignment, practice with sessions, and interacting in the views with them

---
+++ learning_assignments/ninja_gold_app_folder/ninja_gold/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render, redirect
+import random
+
+
+def index(request):
+    if 'gold' not in request.session:
+        request.session['gold'] = 0
+    if 'activity' not in request.session:
+        request.session['activity'] = []
+    return render(request, 'ninja_gold/index.html')
+
+def process(request, place):
+    session_list = request.session['activity']
+    string = ''
+    lost_gain = 'gain'
+    if place == 'farm':
+        num = random.randint(10 , 20)
+        string = 'Earned {} gold from the Farm!'.format(num)
+    if place == 'cave':
+        num = random.randint(5 , 10)
+        string = 'Earned {} gold from the Cave!'.format(num)
+    if place == 'house':
+        num = random.randint(2, 5)
+        string = 'Earned {} gold from the House!'.format(num)
+    if place == 'casino':
+        chance = random.randint(0, 1)
+        num = random.randint(0, 50)
+        if chance:
+            string = 'Earned {} gold from the Casino!'.format(num)
+        else:
+            string = 'Lost {} gold from the Farm!'.format(num)
+            lost_gain = 'lost'
+    if lost_gain == 'gain':
+        request.session['gold'] += num
+    else:
+        request.session['gold'] -= num
+    session_list.append({'string': string, 'lost_gain':lost_gain })
+    request.session['activity'] = session_list
+    return redirect('/')
+
+def reset(request):
+    request.session['gold'] = 0
+    request.session['activity'] = []
+    return redirect('/')
+
+# Create your views here.
